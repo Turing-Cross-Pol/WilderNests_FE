9f465c129c098aa4dbb78b49612332b08f3c6512
Lili Manrique <liliana.manrique1@gmail.com>
2020-05-28 17:32:50 -0600

    Update configurations

--- Landing.js
+++
@@ -1,6 +0,0 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, Image, ImageBackground } from 'react-native';
-
-export const Landing = () => {
-  return(<Text>Hi</Text>)
-}
