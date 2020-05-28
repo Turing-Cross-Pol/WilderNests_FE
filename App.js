20a9a515c07d54f83b13a8aa6636611ad93baf0b
Lili Manrique <liliana.manrique1@gmail.com>
2020-05-28 16:45:23 -0600

    Configure project using Expo. Includes Jest and React Navigation.
    Co-authored-by: Harry Borrelli <hborrelli@outlook.com>

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import {Landing} from './Landing'
 
-export default function App() {
+const Stack = createStackNavigator();
+
+export const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Landing" >
+        <Stack.Screen name="Home" component={Landing} options={{headerShown: false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
+export default App;
 
 const styles = StyleSheet.create({
   container: {
---
+++ Landing.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, Image, ImageBackground } from 'react-native';
+
+export const Landing = () => {
+  return(<Text>Hi</Text>)
+}
