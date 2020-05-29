4077cdbca9c832d1b109e15d26c04a3a6a7c48fb
Lili Manrique <liliana.manrique1@gmail.com>
2020-05-28 21:09:10 -0600

    Destructure route.params to access site properties

--- src/SiteDetails/SiteDetails.js
+++ src/SiteDetails/SiteDetails.js
@@ -2,33 +2,31 @@ import React from "react";
 import { Text, Image, View, StyleSheet } from "react-native";
 
 export const SiteDetails = ({ route }) => {
-  console.log(route)
-  const image = route.image ? route.image : 'https://place-hold.it/300x500'
-
+  const {image, name, city, state, lat, long, description, drivingTips, timestamps } = route.params;
+  const photo = image ? image : 'https://place-hold.it/300x500'
   return (
-    <View>
+    <View style={styles.container}>
       <Image
         style={styles.image}
         source={{
-          uri: image,
+          uri: photo,
         }}
       />
-      <Text>{route.name}</Text>
-      <Text>{route.city}</Text>
-      <Text>{route.state}</Text>
-      <Text>{route.lat}</Text>
-      <Text>{route.long}</Text>
-      <Text>{route.description}</Text>
-      <Text>{route.drivingTips}</Text>
-      <Text>Date added: {route.timestamps}</Text>
+      <Text>{name}</Text>
+      <Text>{city}</Text>
+      <Text>{state}</Text>
+      <Text>{lat}</Text>
+      <Text>{long}</Text>
+      <Text>{description}</Text>
+      <Text>{drivingTips}</Text>
+      <Text>Date added: {timestamps}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  listItem: {
-    backgroundColor: '#333',
-    padding: 10
+  container: {
+    flex: 1
   },
   image: {
     width: 100,
