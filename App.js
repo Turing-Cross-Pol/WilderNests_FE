1f94c22615565003a85c720408573eb061e06459
Lili Manrique <liliana.manrique1@gmail.com>
2020-05-29 14:12:44 -0600

    Add styling for header.
    Co-authored-by: Harry Borrelli <hborrelli@outlook.com>

--- App.js
+++ App.js
@@ -1,14 +1,31 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { COLORS } from './assets/constants/constants';
 import { Landing } from "./src/Landing/Landing";
 import { PostForm } from "./src/PostForm/PostForm";
-import { ListView } from "./src/ListView/ListView";
+import { ToggleView } from "./src/ToggleView/ToggleView";
 import { SiteDetails } from "./src/SiteDetails/SiteDetails";
+
 
 const Stack = createStackNavigator();
 
 const App = () => {
+
+  const brandHeader = {
+    title: 'WilderNests',
+    headerStyle: {
+      backgroundColor: COLORS.green,
+    },
+    headerTitleStyle: {
+      color: '#fff',
+    },
+    headerBackTitleStyle: {
+      color: '#fff',
+    },
+    headerTintColor: '#fff',
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Landing">
@@ -18,8 +35,16 @@ const App = () => {
           options={{ headerShown: false }}
         />
         <Stack.Screen name="Post" component={PostForm} />
-        <Stack.Screen name="List View" component={ListView} />
-        <Stack.Screen name="Details" component={SiteDetails} />
+        <Stack.Screen 
+          name="Toggle View" 
+          component={ToggleView} 
+          options={brandHeader} 
+        />
+        <Stack.Screen 
+          name="Details" 
+          component={SiteDetails} 
+          options={brandHeader}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
