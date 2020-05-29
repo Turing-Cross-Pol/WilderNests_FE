7e9cc27e93cccae900651017b5db55526737ee4f
Lili Manrique <liliana.manrique1@gmail.com>
2020-05-29 14:13:56 -0600

    Remove toggle view from component.
    Co-authored-by: Harry Borrelli <hborrelli@outlook.com>

--- src/ListView/ListView.js
+++ src/ListView/ListView.js
@@ -1,35 +1,26 @@
 import React, { useState } from "react";
 import { data } from "../../sample-data.js";
 import { ListCard } from "../ListCard/ListCard";
-import { ToggleView } from '../ToggleView/ToggleView';
 import {
-  SafeAreaView,
   FlatList,
   StyleSheet,
-  TouchableOpacity,
-  Text,
   View
 } from "react-native";
 
 export const ListView = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <ToggleView currentPage='listView' />
+    <View style={styles.container}>
       <FlatList
         data={data.data}
         renderItem={({ item }) => <ListCard info={item} />}
         keyExtractor={(item) => item.id}
       />
-    </SafeAreaView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  buttonBox: {
-    display: "flex",
-    flexDirection: "row"
   }
 });
