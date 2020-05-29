9b5754caec164dbfbce6c6d272858c37a2d82a97
Lili Manrique <liliana.manrique1@gmail.com>
2020-05-29 16:34:01 -0600

    Add additional render tests for landing page.
    Co-authored-by: Harry Borrelli <hborrelli@outlook.com>

--- src/Landing/Landing.test.js
+++ src/Landing/Landing.test.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import { render, waitFor } from 'react-native-testing-library';
+import React from "react";
+import { render, waitFor } from "react-native-testing-library";
 
-import { Landing } from './Landing';
+import { Landing } from "./Landing";
 
-
-describe('Landing Component', () => {
-  test('Renders to the screen', async () => {
+describe("Landing Component", () => {
+  test("Renders the App name", async () => {
     const { getByText } = render(<Landing />);
-    
-    await waitFor(() => getByText('WilderNests'));
-    expect(getByText('WilderNests')).toBeTruthy();
+
+    await waitFor(() => getByText("WilderNests"));
+    expect(getByText("WilderNests")).toBeTruthy();
+  });
+
+  test("Renders the Find and Post buttons", async () => {
+    const { getByText } = render(<Landing />);
+
+    const find = await waitFor(() => getByText("Find a Campsite"));
+    const post = await waitFor(() => getByText("Post a Campsite"));
+    expect(find).toBeTruthy();
+    expect(post).toBeTruthy();
   });
 });
