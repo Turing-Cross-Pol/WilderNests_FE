9a7fd037b65f1960738d2624540d427947840324
Lili Manrique <liliana.manrique1@gmail.com>
2020-05-28 18:10:12 -0600

    Pull in sample data file from old repo.
    Co-authored-by: Harry Borrelli <hborrelli@outlook.com>

---
+++ sample-data.js
@@ -0,0 +1,65 @@
+export const data = {
+  data: [
+    {
+      id: 1,
+      city: 'Idaho Springs',
+      state: 'Colorado',
+      Name: 'Dispersed Camping Near St. Mary\'s Glacier',
+      Long: '-105.626933',
+      Lat: '39.799558',
+      Description: 'Dispersed campsites are all along Fall River Rd. We saw at least 4 other vehicles camping 20 to 100 feet off the road. There are also a dozen or so areas to pull off on the shoulder as well. Expect mostly level dirt paths or shoulder areas and look for stone fire rings marking the dispersed sites. There are no amenities (no restrooms, water, etc.). We stayed several days and were not bothered. See http://www.fs.usda.gov/Internet/FSE_DOCUMENTS/stelprdb5165771.pdf for exact site details.',
+      DrivingTips: '',
+      Image: 'https://freecampsites.net/wp-content/uploads/2019/06/Idaho-Falls-2-800x600.jpg',
+      Timestamps: 'May 13, 2018',
+    },
+    {
+      id: 2,
+      city: 'Idaho Springs',
+      state: 'Colorado',
+      Name: 'Yankee Hill',
+      Long: '39.80458',
+      Lat: '-105.56873',
+      Description: 'Yankee Hill Road is a 15.4 mile moderately trafficked out and back trail located near Idaho Springs, Colorado that offers scenic views. The trail is rated as moderate and primarily used for off road driving. Plenty of space to camp along the trail, but expect traffic.',
+      DrivingTips: 'The dirt road that leads to the campsite is rated as moderate and primarily used for off road driving. 4-wheel drive is probably needed.',
+      Image: '',
+      Timestamps: 'Aug 5, 2018',
+    },
+    {
+      id: 3,
+      city: 'Idaho Springs',
+      state: 'Colorado',
+      Name: 'Camping on Mill Creek Rd',
+      Long: '39.78307',
+      Lat: '-105.6235',
+      Description: 'I have not been here but I have been to other dispersed campsites nearby on Fall River Rd. There are numerous dispersed campsites (primitive) listed here by the Forest Service: https://www.fs.usda.gov/Internet/FSE_DOCUMENTS/stelprdb5165771.pdf',
+      DrivingTips: '',
+      Image: '',
+      Timestamps: 'May 13, 2018',
+    },
+    {
+      id: 4, 
+      city: '',
+      state: '',
+      Name: 'Bald Mountain Ln West of Central City, CO',
+      Long: '39.795211',
+      Lat: '-105.545953',
+      Description: 'I have not been here, but the Forest Service dispersed camping along the road West of this point. The road quality leading up to these sites appears good, but be aware that dispersed sites are primitive with no restrooms or facilities apart from a firepit. https://www.fs.usda.gov/Internet/FSE_DOCUMENTS/stelprdb5165771.pdf',
+      DrivingTips: '',
+      Image: '',
+      Timestamps: 'May 13, 2018',
+    }
+  ]
+}
+
+
+// City.String
+// State.String
+// Name.string
+// Long.Float
+// Lat.Float
+// Description.string
+// Driving Tips.string
+// Image.string(url for now. upload an image as a stretch goal)
+// Timestamps
+// has_many comments
+// has_many amenities
