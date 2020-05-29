f90101b9d9bf8e91757b9c2e1813726e5e7a6a62
Lili Manrique <liliana.manrique1@gmail.com>
2020-05-29 11:44:47 -0600

    Move toggle navigation out of list view into its own component and pass it a current page prop.
    Co-authored-by: Harry Borrelli <hborrelli@outlook.com>

--- src/ListView/ListView.js
+++ src/ListView/ListView.js
@@ -1,23 +1,35 @@
-import React, { useState } from 'react';
-import { data } from '../../sample-data.js'
+import React, { useState } from "react";
+import { data } from "../../sample-data.js";
 import { ListCard } from "../ListCard/ListCard";
-import { SafeAreaView, FlatList, StyleSheet } from 'react-native'
+import { ToggleView } from '../ToggleView/ToggleView';
+import {
+  SafeAreaView,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  View
+} from "react-native";
 
 export const ListView = () => {
-
   return (
     <SafeAreaView style={styles.container}>
+      <ToggleView currentPage='listView' />
       <FlatList
         data={data.data}
-        renderItem={({ item }) => <ListCard info={ item } />}
-        keyExtractor={item => item.id}
+        renderItem={({ item }) => <ListCard info={item} />}
+        keyExtractor={(item) => item.id}
       />
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  buttonBox: {
+    display: "flex",
+    flexDirection: "row"
+  }
 });
