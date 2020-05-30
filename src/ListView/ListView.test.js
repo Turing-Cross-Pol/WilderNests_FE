db1a4406364fa24c45dcc1042345a5c34ec2e96e
Harry Borrelli <hborrelli@outlook.com>
2020-05-30 15:04:54 -0600

    Fix test issues on ListView component.

--- src/ListView/ListView.test.js
+++ src/ListView/ListView.test.js
@@ -1,13 +1,32 @@
 import React from "react";
 import { render, waitFor } from "react-native-testing-library";
+import { data } from '../../sample-data'
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 import { ListView } from "./ListView";
 
 describe("ListView", () => {
-  test("Renders the App name", async () => {
-    const { getByTestId } = render(<ListView />);
+  let Stack;
 
-    const container = await waitFor(() => getByTestId("list container"));
-    expect(container).toBeTruthy();
+  beforeEach(() => {
+    Stack = createStackNavigator();
+  })
+
+  test("Renders the App name", async () => {
+    const { getByText } = render(
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="List View">
+          <Stack.Screen
+            name="List View"
+            component={() => <ListView data={data.data} />}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+
+    const site1 = await waitFor(() => getByText("Dispersed Camping Near St. Mary's Glacier"));
+    expect(site1).toBeTruthy();
+    expect(true).toEqual(true)
   });
 });
