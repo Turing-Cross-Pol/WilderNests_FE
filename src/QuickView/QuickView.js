0f9832b8141452444c5244bc300707db73a44142
Lili Manrique <liliana.manrique1@gmail.com>
2020-05-30 13:19:03 -0600

    Style quickview component.
    Co-authored-by: Harry Borrelli <hborrelli@outlook.com>

--- src/QuickView/QuickView.js
+++ src/QuickView/QuickView.js
@@ -35,4 +35,44 @@ export const QuickView = ({ campsite }) => {
   );
 };
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexGrow: 1,
+    backgroundColor: "#f1f1f1",
+    position: "absolute",
+    bottom: -30,
+    paddingTop: 30,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingBottom: 60,
+    left: 0,
+    right: 0,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 8.84,
+  },
+  name: {
+    fontFamily: "MavenPro-Medium",
+    fontSize: 25,
+    color: COLORS.green,
+    marginBottom: 5,
+  },
+  location: {
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  description: {
+    marginBottom: 20,
+  },
+  moreDetails: {
+    textAlign: "center",
+    color: COLORS.purple,
+    fontFamily: 'MavenPro-Medium',
+    letterSpacing: 1,
+  }
+});
