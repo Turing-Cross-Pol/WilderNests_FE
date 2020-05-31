701236c266b7941f27703f9ff77c62b32dc7b6d4
Lili Manrique <liliana.manrique1@gmail.com>
2020-05-30 22:30:26 -0600

    Manage state for form and handle press of star icons

--- src/CommentForm/CommentForm.js
+++ src/CommentForm/CommentForm.js
@@ -1,12 +1,93 @@
-import React, { useState } from 'React';
-import { SafeAreaView } from 'react-native'
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  TextInput,
+  FlatList,
+  TouchableOpacity,
+  Text,
+  Image,
+  StyleSheet,
+} from "react-native";
 
-export const CommentForm = ({ rating }) => {
-  const [userRating, setUserRating] = useState(rating);
+export const CommentForm = ({ route }) => {
+  const { newRating, name, id } = route.params;
+  const [rating, setRating] = useState(newRating);
+  const [description, setDescription] = useState("");
+  const [title, setTitle] = useState("");
 
-  return(
+  const createStarDisplay = (rating) => {
+    const numStars = rating ? Math.ceil(rating) : 0;
+    const filledStars = Array(numStars).fill(
+      require("../../assets/images/filled-star.png")
+    );
+    const emptyStars = Array(5 - numStars).fill(
+      require("../../assets/images/empty-star.png")
+    );
+    return filledStars.concat(emptyStars);
+  };
+
+  const initialStars = createStarDisplay(rating);
+  const [stars, setStars] = useState(initialStars);
+
+  const handleRating = (index) => {
+    const newRating = index + 1;
+    setRating(newRating);
+    const newStars = createStarDisplay(newRating);
+    setStars(newStars);
+  };
+
+  return (
     <SafeAreaView>
-      
+      <FlatList
+        style={styles.flatList}
+        numColumns={5}
+        data={stars}
+        renderItem={({ item, index }) => (
+          <TouchableOpacity onPress={() => handleRating(index)}>
+            <Image source={item} key={index} style={styles.star} />
+          </TouchableOpacity>
+        )}
+        keyExtractor={(item) => item.key}
+      />
+      <Text>Rating for {name}</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Comment Title"
+        value={title}
+        onChangeText={(newTitle) => setTitle(newTitle)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Please provide any comments about this site that may be helpful to other visitors"
+        multiline={true}
+        numberOfLines={4}
+        value={description}
+        onChangeText={(newComment) => setDescription(newComment)}
+      />
+      <TouchableOpacity>
+        <Text>Submit Comment</Text>
+      </TouchableOpacity>
     </SafeAreaView>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  star: {
+    height: 25,
+    width: 25,
+    marginRight: 3,
+  },
+  flatList: {
+    alignSelf: "center",
+    margin: 20
+  },
+  input: {
+    fontSize: 20,
+    padding: 15,
+    borderColor: "#537A72",
+    borderWidth: 1,
+    marginLeft: 20,
+    marginRight: 20,
+    marginBottom: 5,
+  },
+});
