b73d8f69bf57bbf31a7881e4cc34f3b7cd1112b3
Lili Manrique <liliana.manrique1@gmail.com>
2020-05-30 23:05:00 -0600

    Add a comment button to navigate to comment form.

--- src/SiteDetails/SiteDetails.js
+++ src/SiteDetails/SiteDetails.js
@@ -10,6 +10,7 @@ import {
 } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { useNavigation } from "@react-navigation/native";
+import { COLORS } from "../../assets/constants/constants";
 
 export const SiteDetails = ({ route }) => {
   const navigation = useNavigation();
@@ -25,7 +26,7 @@ export const SiteDetails = ({ route }) => {
     driving_tips,
     timestamps,
     rating,
-    id
+    id,
   } = route.params;
   const photo = image_url ? image_url : "https://place-hold.it/300x500";
 
@@ -82,11 +83,15 @@ export const SiteDetails = ({ route }) => {
       <Text style={styles.header}>Driving Tips:</Text>
       <Text style={styles.text}>{driving_tips}</Text>
       <Text style={styles.text}>Date added: {timestamps}</Text>
-      <Button
-        onPress={getDirections}
-        title="Get Directions"
-        color={"#7E62CF"}
-      />
+      <TouchableOpacity
+        style={styles.touchable}
+        onPress={() => navigation.navigate("Comment Form", { name, id })}
+      >
+        <Text style={styles.button}>Write a Comment/Review</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.touchable} onPress={getDirections}>
+        <Text style={styles.button}>Get Directions</Text>
+      </TouchableOpacity>
     </ScrollView>
   );
 };
@@ -119,5 +124,19 @@ const styles = StyleSheet.create({
     height: 200,
     marginLeft: 20,
     marginRight: 20,
+  },
+  touchable: {
+    borderRadius: 4,
+    backgroundColor: COLORS.purple,
+    width: 300,
+    alignSelf: "center",
+    marginTop: 20,
+  },
+  button: {
+    color: "#fff",
+    padding: 15,
+    textAlign: "center",
+    fontFamily: "MavenPro-Medium",
+    fontSize: 20,
   },
 });
