52d0bf250800d2b63cace9468aed88045234294b
Lili Manrique <liliana.manrique1@gmail.com>
2020-05-31 12:54:07 -0600

    Write basic render test for toggle view component

--- src/ToggleView/ToggleView.test.js
+++ src/ToggleView/ToggleView.test.js
@@ -1,12 +1,36 @@
+
+
 import React from "react";
 import { render, waitFor } from "react-native-testing-library";
+import { data } from '../../sample-data'
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 import { ToggleView } from "./ToggleView";
 
 describe("ToggleView", () => {
-  test("Renders the App name", async () => {
-    const { getByText } = render(<ToggleView />);
+  let Stack;
 
+  beforeEach(() => {
+    Stack = createStackNavigator();
+  })
+
+  test("Renders what we expect", async () => {
+    const toggleComponent = () => (
+      <ToggleView data={data.data} />
+    )
+
+    const { getByText } = render(
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Toggle View">
+          <Stack.Screen
+            name="Toggle View"
+            component={toggleComponent}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+    
     const list = await waitFor(() => getByText("List View"));
     const map = await waitFor(() => getByText("Map View"));
     expect(list).toBeTruthy();
