4e932e934bbb6e9e5a4cc48948c6b6804e063ac2
Lili Manrique <liliana.manrique1@gmail.com>
2020-05-31 13:11:14 -0600

    Fix spacing and formatting

--- src/ToggleView/ToggleView.test.js
+++ src/ToggleView/ToggleView.test.js
@@ -1,8 +1,6 @@
-
-
 import React from "react";
 import { render, waitFor } from "react-native-testing-library";
-import { data } from '../../sample-data'
+import { data } from "../../sample-data";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
@@ -13,24 +11,19 @@ describe("ToggleView", () => {
 
   beforeEach(() => {
     Stack = createStackNavigator();
-  })
+  });
 
   test("Renders what we expect", async () => {
-    const toggleComponent = () => (
-      <ToggleView data={data.data} />
-    )
+    const toggleComponent = () => <ToggleView data={data.data} />;
 
     const { getByText } = render(
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Toggle View">
-          <Stack.Screen
-            name="Toggle View"
-            component={toggleComponent}
-          />
+          <Stack.Screen name="Toggle View" component={toggleComponent} />
         </Stack.Navigator>
       </NavigationContainer>
     );
-    
+
     const list = await waitFor(() => getByText("List View"));
     const map = await waitFor(() => getByText("Map View"));
     expect(list).toBeTruthy();
