eac1c185adc46abdd1602747a739567cb8593ce9
Lili Manrique <liliana.manrique1@gmail.com>
2020-05-30 22:00:03 -0600

    Add comment form component, set rating state using rating prop

---
+++ src/CommentForm/CommentForm.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'React';
+import { SafeAreaView } from 'react-native'
+
+export const CommentForm = ({ rating }) => {
+  const [userRating, setUserRating] = useState(rating);
+
+  return(
+    <SafeAreaView>
+      
+    </SafeAreaView>
+  )
+}
