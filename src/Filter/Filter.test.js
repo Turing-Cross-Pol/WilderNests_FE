0c3e959406d614d96bc13c6c874a02cb2ff45a1e
Lili Manrique <liliana.manrique1@gmail.com>
2020-06-02 11:17:45 -0600

    Add test for expanding filter view.

--- src/Filter/Filter.test.js
+++ src/Filter/Filter.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, waitFor } from "react-native-testing-library";
+import { render, waitFor, fireEvent } from "react-native-testing-library";
 import { data } from "../../sample-data";
 import { Filter } from "./Filter";
 import { NavigationContainer } from "@react-navigation/native";
@@ -12,14 +12,14 @@ describe("Filter Test", () => {
 
   beforeEach(() => {
     Stack = createStackNavigator();
-    let options = data.data.filter((data) => data.city && data.state);
+    options = data.data.filter((data) => data.city && data.state);
     options = options.map((data) => data.city + ", " + data.state);
     options = [...new Set(options)];
     options = options.sort();
   });
 
   test("Renders what we expect", async () => {
-    const { getByText, getByTestId } = render(
+    const { getByText } = render(
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen
@@ -35,5 +35,29 @@ describe("Filter Test", () => {
     );
 
     expect(filterButton).toBeTruthy();
+  });
+  test("Expands when clicked", async () => {
+    const { getByText, getByTestId } = render(
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Filter"
+            component={() => <Filter setSelected={jest.fn()} options={options}/>}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+
+    const filterButton = await waitFor(() =>
+      getByText("Filter (expand)")
+    );
+    fireEvent.press(filterButton);
+    const idahoSprings = await waitFor(() =>
+      getByText("Idaho Springs, CO")
+    );
+    const expanded = await waitFor(() =>
+      getByText("Filter (hide)")
+    );
+    expect(expanded).toBeTruthy();
   });
 });
