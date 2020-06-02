10ec0ecfa7a214990442b5ac29c284c4db2fff3e
Lili Manrique <liliana.manrique1@gmail.com>
2020-06-02 09:47:38 -0600

    Add basic filter component that hides and shows rest of view on click.

--- src/Filter/Filter.js
+++ src/Filter/Filter.js
@@ -1,6 +1,70 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState } from "react";
+import { Text, TouchableOpacity, View, StyleSheet, Image } from "react-native";
+import { COLORS } from "../../assets/constants/constants";
 
 export const Filter = () => {
-  return <Text>Hi</Text>;
+  const [expanded, setExpanded] = useState("");
+  const [selected, setSelected] = useState("");
+  const handleFilter = (selected) => {
+    setSelected(selected);
+  };
+  const emptyCheck = require("../../assets/images/checkbox.png");
+  const fullCheck = require("../../assets/images/done.png");
+
+  const toggleExpanded = () => {
+    if (expanded) {
+      setExpanded("");
+    } else {
+      setExpanded("expanded");
+    }
+  };
+
+  return (
+    <View>
+      <TouchableOpacity style={styles.touchable} onPress={toggleExpanded}>
+  <Text style={styles.button}>Filter {!!expanded && "(hide)"}</Text>
+      </TouchableOpacity>
+      {!!expanded && (
+        <View>
+          <TouchableOpacity
+            style={styles.checkContainer}
+            onPress={() => handleFilter("horse")}
+            testID="Horse Trails"
+          >
+            <Image
+              style={styles.icon}
+              // source={amenities.includes("horse") ? fullCheck : emptyCheck}
+            />
+            <Text style={styles.label}>Horse Trails</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  label: {
+    fontSize: 20,
+    marginLeft: 20,
+    color: COLORS.green,
+  },
+  checkContainer: {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "row",
+    marginLeft: 20,
+    marginTop: 10,
+  },
+  button: {
+    // color: COLORS.green,
+    padding: 5,
+    textAlign: "center",
+    fontFamily: "MavenPro-Medium",
+    fontSize: 20,
+  },
+  touchable: {
+    backgroundColor: COLORS.cream,
+    alignSelf: "stretch",
+  },
+});
