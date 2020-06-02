e0fe8ecdd019ebc0e040f8c8fe2468da3fe59967
Lili Manrique <liliana.manrique1@gmail.com>
2020-06-02 10:11:24 -0600

    Add filter logic for selected city.

--- src/ListView/ListView.js
+++ src/ListView/ListView.js
@@ -2,20 +2,26 @@ import React, { useState } from "react";
 // import { data } from "../../sample-data.js";
 import { ListCard } from "../ListCard/ListCard";
 import { Filter } from "../Filter/Filter";
-import {
-  FlatList,
-  StyleSheet,
-  View
-} from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 
 export const ListView = ({ data }) => {
+  const [selected, setSelected] = useState("");
+
+  let options = data.filter((data) => data.city && data.state);
+  options = options.map((data) => data.city + ", " + data.state);
+  options = [...new Set(options)];
+
+  const display = selected
+    ? data.filter((data) => data.city + ", " + data.state === selected)
+    : data;
+
   return (
     <View style={styles.container}>
-      <Filter /> 
+      <Filter setSelected={setSelected} options={options} />
       <FlatList
-        data={data}
+        data={display}
         renderItem={({ item }) => <ListCard info={item} />}
-        keyExtractor={item => item.id.toString()}
+        keyExtractor={(item) => item.id.toString()}
       />
     </View>
   );
@@ -24,5 +30,5 @@ export const ListView = ({ data }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  }
+  },
 });
